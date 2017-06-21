dfc2bbc4a2d5c1a278c93a7eab7b22b5b40b7b6b
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-21 13:37:19 -0400

    get tweenMax test func working

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'; 
+import { TweenMax, Power2, TimelineLite } from 'gsap';
 
 class Banner extends Component {
+  constructor() {
+    super();
+  };
+  componentDidMount() {
+    this.tweenMax();
+  };
+  tweenMax() {
+    TweenMax.to(".target", 1, {marginLeft:10, backgroundColor: "red"});
+  };
   render() {
     return(
       <div className="banner">
+        <p className="target">Poo Poo Pee Pee</p>
       </div>
     )
   }
