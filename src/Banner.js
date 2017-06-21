0d1528119131cd0e01c54be0fd34484e6cb13913
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-21 14:44:33 -0400

    get 2/3 of the animation working

--- src/Banner.js
+++ src/Banner.js
@@ -9,15 +9,49 @@ class Banner extends Component {
     this.tweenMax();
   };
   tweenMax() {
+    TweenMax.to(".target", 2, {
+      delay: 0.75,
+      bottom: 330,
+      right: 150,
+      scale: 1,
+    });
+    //pan slightly left while zooming to reveal lower half of cup
     TweenMax.to(".target", 1, {
-      marginLeft:10, 
-      backgroundColor: "red",
+      delay: 2.75,
+      right: 200,
+      bottom: 375,
+      scale: 0.5,
     });
     TweenMax.to(".target", 1, {
-        marginLeft:"-10%", 
-        delay: 1.5,
-        backgroundColor: "blue", }
-      );
+      delay: 3.75,
+      right: 50,
+    });
+    TweenMax.to(".f4Copy", 1, {
+      delay: 3.75,
+      opacity: 1, 
+    });
+    TweenMax.to(".f4Copy", 0.1, {
+      delay: 5.75,
+      opacity: 0
+    });
+    TweenMax.to(".target", 1, {
+      delay: 5.75,
+      scale: 0.2,
+      right: 150,
+      bottom: 275,
+    });
+    TweenMax.to(".f5Copy1", 0.75, {
+      delay: 6.75,
+      opacity: 1,
+    });
+    TweenMax.to(".f5Copy1", 1, {
+      delay: 7.25,
+      left: 0,
+    });
+    TweenMax.to(".f5Copy2", 1, {
+      delay: 7.25, 
+      opacity: 1,
+    });
   };
   render() {
     return(
@@ -25,6 +59,12 @@ class Banner extends Component {
         <img 
           className="target"
           src="./assets/product.jpg"/>
+        <img className="f4Copy"
+          src="./assets/f4_copy.png"/>
+        <img className="f5Copy1"
+          src="./assets/f5_copy1.png"/>
+        <img className="f5Copy2"
+          src="./assets/f5_copy2.png"/>
       </div>
     )
   }
